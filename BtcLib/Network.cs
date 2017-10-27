5bdb53fe2f784344adbd6e7a0d2d370852e463e8
Ron O'Hara <acid1789@gmail.com>
2017-10-26 23:04:41 -0700

    Initial

---
+++ BtcLib/Network.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtcLib
+{
+    public class Network
+    {
+        const int BitcoinPort = 8333;
+        string[] BitcoinSeeds = { "seed.bitcoin.sipa.be",
+                                    "dnsseed.bluematt.me",
+                                    "dnsseed.bitcoin.dashjr.org",
+                                    "seed.bitcoinstats.com",
+                                    "seed.bitcoin.jonasschnelli.ch",
+                                    "seed.btc.petertodd.org" };
+
+
+    }
+}
